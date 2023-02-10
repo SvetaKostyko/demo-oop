0ce9286ce884d18a61406276c29967bfaf8ae925
Sveta <skostyko@gmail.com>
2023-02-10 15:36:17 +0100

    add class Customer

---
+++ src/main/java/by/itacademy/svetakostyko/javabasic/oop/Customer.java
@@ -0,0 +1,38 @@
+package by.itacademy.svetakostyko.javabasic.oop;
+
+public class Customer {
+    private int id;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String address;
+    private int numberOfCard;
+    private long numberOfBankAccount;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setNumberOfCard(int numberOfCard) {
+        this.numberOfCard = numberOfCard;
+    }
+
+    public void setNumberOfBankAccount(long numberOfBankAccount) {
+        this.numberOfBankAccount = numberOfBankAccount;
+    }
+}
