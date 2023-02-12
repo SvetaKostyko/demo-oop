371cc1cfab7b259d56c740d6472fae979e83fa25
Sveta <skostyko@gmail.com>
2023-02-12 22:46:58 +0100

    refactor Demo

--- src/main/java/by/itacademy/svetakostyko/javabasic/oop/DemoOOP.java
+++ src/main/java/by/itacademy/svetakostyko/javabasic/oop/DemoOOP.java
@@ -14,28 +14,17 @@ public class DemoOOP {
 
 
         Book book1 = new Book(10857656, "Ten little Indians", "Agatha Kristy", "Eksmo", 2021, 288, 11.84);
-        Book book2 = new Book(10857658, "The Witches of Eastwick", "John Updike", "Kapo", 	2019, 192, 13.84);
+        Book book2 = new Book(10857658, "The Witches of Eastwick", "John Updike", "Kapo", 2019, 192, 13.84);
         System.out.println(book1);
         System.out.println(book2);
         System.out.println(book1.equals(book2));
 
 
-     /*   Car car = new Car();
-        car.setId(103174396);
-        car.setCarBrand("Skoda");
-        car.setModelOfCar("Kodiaq");
-        car.setYearOfProduction(2018);
-        car.setColor("blue");
-        car.setPrice(77_400);
-        car.setRegistrationNumber("6325CH5");
-        System.out.println(car.getId());
-        System.out.println(car.getCarBrand());
-        System.out.println(car.getModelOfCar());
-        System.out.println(car.getYearOfProduction());
-        System.out.println(car.getColor());
-        System.out.println(car.getPrice());
-        System.out.println(car.getRegistrationNumber());
-*/
+        Car carSkoda = new Car(103174396, "Skoda", "Kodiaq", 2018, "blue", 77_400, "6325CH5");
+        Car carMazda = new Car(103174368, "Mazda", "6", 2018, "red", 68_000, "5896KP7");
+        System.out.println(carSkoda);
+        System.out.println(carMazda);
+        System.out.println(carSkoda.equals(carMazda));
     }
 
 }
