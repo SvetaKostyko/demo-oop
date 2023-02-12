5ee37be003ace313898ff7d3fc9db2913f486107
Sveta <skostyko@gmail.com>
2023-02-12 17:38:36 +0100

    refactor Car

--- src/main/java/by/itacademy/svetakostyko/javabasic/oop/Car.java
+++ src/main/java/by/itacademy/svetakostyko/javabasic/oop/Car.java
@@ -9,57 +9,26 @@ public class Car {
     private double price;
     private String registrationNumber;
 
-    public void setId(int id) {
+    public Car(int id, String carBrand, String modelOfCar, int yearOfProduction, String color, double price, String registrationNumber) {
         this.id = id;
-    }
-
-    public void setCarBrand(String brand) {
-        carBrand = brand;
-    }
-    public void setModelOfCar(String model) {
-        modelOfCar = model;
-    }
-
-    public void setYearOfProduction(int yearOfProduction) {
+        this.carBrand = carBrand;
+        this.modelOfCar = modelOfCar;
         this.yearOfProduction = yearOfProduction;
-    }
-
-    public void setColor(String color) {
         this.color = color;
-    }
-
-    public void setPrice(double price) {
         this.price = price;
-    }
-    public void setRegistrationNumber(String registrationNumber) {
         this.registrationNumber = registrationNumber;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getCarBrand() {
-        return carBrand;
-    }
-
-    public String getModelOfCar() {
-        return modelOfCar;
-    }
-
-    public int getYearOfProduction() {
-        return yearOfProduction;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public String getRegistrationNumber() {
-        return registrationNumber;
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", carBrand='" + carBrand + '\'' +
+                ", modelOfCar='" + modelOfCar + '\'' +
+                ", yearOfProduction=" + yearOfProduction +
+                ", color='" + color + '\'' +
+                ", price=" + price +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                '}';
     }
 }
